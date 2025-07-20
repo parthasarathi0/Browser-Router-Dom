83afca18d36d3001473ebd115de0c418774654d9
parthasarathi0 <sarathisree001@gmail.com>
2025-07-20 21:55:46 +0530

    added navigate hook

--- src/assets/pages/Navbar.jsx
+++ src/assets/pages/Navbar.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 const Navbar = () => {
+  let navigation = useNavigate();
+let handleClick = ()=>{
+    navigation("/About")
+}
   return (
-
-<nav>
-    <h1>
-        Brand
-    </h1>
-    <ul>
+    <nav>
+      <h1>Brand</h1>
+      <ul>
         <Link to="/">Home</Link>
-        <Link to="/About">About</Link>
+        {/* <Link to="/About">About</Link> */}
+        <li
+          onClick={handleClick}
+        >
+          about
+        </li>
         <Link to="/User">User</Link>
         <Link to="/Location">Location</Link>
         <Link to="/Contact">Contact</Link>
-    </ul>
-</nav>
-)
-}
+      </ul>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
